706151ed4ce28c7e903fa224d0e286dabe6a48fa
roshanjha13 <jharoshan618@gmail.com>
2025-06-04 23:05:42 +0530

    warm up class 1

---
+++ warmup/ex1.js
@@ -0,0 +1,53 @@
+// console.log("hello world");
+// console.log(7);
+// console.log(true);
+
+// const a = 10;
+
+// const b = 20;
+
+// const sum = a+b
+// const diff = a - b
+// const mul = a * b
+// console.log(sum);
+// console.log(diff);
+// console.log(mul);
+
+
+// let firstName = "Roshan"
+// let lastName = "Jha"
+
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName);
+
+// let arr = [2,6,0,100,9,27]
+
+// let sum = arr[0] + arr[1]
+// console.log(sum);
+
+// let strArr = ["roshan","rahul","koushik"]
+
+// console.log(strArr[0]);
+
+
+// let mixArr = ["roshan",99,true,"rahul",10]
+
+// console.log(mixArr);
+
+// let arrInArr = ["roshan",7,[5,10,[2,30]]]
+
+// console.log(arrInArr[2][1]);
+
+
+let obj = {
+    id:1,
+    name:"roshan",
+    bool:true,
+    hobbies:["cricket","learning"]
+}
+
+console.log(obj.bool);
+console.log(obj.id);
+console.log(obj.name);
+console.log(obj.hobbies[0] + " " + obj.hobbies[1]);
