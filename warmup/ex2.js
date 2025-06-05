45c3566374b5d69d2defaca2a7084fb91f4be8e3
roshanjha13 <jharoshan618@gmail.com>
2025-06-05 22:50:36 +0530

    day 2 complete function and if esle

---
+++ warmup/ex2.js
@@ -0,0 +1,65 @@
+// function printHelloWorld() { 
+//     console.log("hello world");
+// }
+
+// printHelloWorld()
+
+// function greet(name) {
+//     console.log('namaste',name);
+// }
+
+// let x = 'rohit jha'
+// greet('roshan')
+// greet(x)
+
+// function addTwoNum(a,b) {
+//     // let sum = a+b;
+//     // return sum
+//     return a+b
+// }
+
+// console.log(addTwoNum(6,9))
+
+// function mulTwoNum(a,b) {
+//     // let sum = a+b;
+//     // return sum
+//     return a*b
+// }
+
+// console.log(mulTwoNum(6,9))
+
+// function square(num){
+//     return num*num
+// }
+
+// let x = square(10)
+// console.log(x);
+
+// function eligbleToVote(age) {
+//     if (age === 0 || age < 0){
+//         console.log("invalid input");
+//     }
+//     else if (age < 18) {
+//         console.log("not eligble to vote");
+//     } else {
+//         console.log("eligble to vote");
+//     }
+// }
+
+// eligbleToVote(20)
+
+function isEvenOrOdd(num) {
+    if (num === 0 || num < 0){
+        console.log("invalid input");
+    }
+    else if (num % 2 === 0) {
+        console.log("even number");
+    } else {
+        console.log("odd number");
+    }
+}
+
+isEvenOrOdd(-1)
+
+
+
