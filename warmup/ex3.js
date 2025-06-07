2745ef72dbf66681d24e9d178bc5ac8a04b2f91a
roshanjha13 <jharoshan618@gmail.com>
2025-06-08 00:51:53 +0530

    basic_loop

---
+++ warmup/ex3.js
@@ -0,0 +1,59 @@
+// for (let i = 0; i < 20; i++) {
+//     console.log("hello word");
+// }
+
+// for (let i = 3; i < 5; i++) {
+//     console.log("hw");
+// }
+
+// for (let i = 2; i <= 9; i=i+2) {
+//     console.log("hw");
+// }
+
+
+// for (let i = 5; i >0 ; i--) {
+//     console.log("hw");
+// }
+
+// for (let i = 5; i <  4; i--) {
+//     console.log("hw");
+// }
+
+// function greet(i){
+//     console.log("Namaste",i);
+    
+// }
+// for (let i = 0; i < 20; i++) {
+//     greet(i)
+// }
+
+// let arr = [10,6,2,0,8,3]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);    
+// }
+
+//print even number form this array
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]%2==0) {
+//         console.log(arr[i]);
+//     }   
+// }
+
+
+let i = 0
+
+while (i < 5) {
+    // this body of loop
+    console.log('hello');
+    
+    i++
+}
+
+
+
+
+
+
+
