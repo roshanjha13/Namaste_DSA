88532c5667f9fbb828816a0dae434a64e871527a
roshanjha13 <jharoshan618@gmail.com>
2025-06-08 23:04:17 +0530

    countNegativeNumber

---
+++ warmup/countNegativeNumber.js
@@ -0,0 +1,13 @@
+function countNegativeNumber(arr){
+    if (arr.length === 0) return "invalid array"
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            ++count            
+        }    
+    }
+
+    return count
+}
+
+console.log(countNegativeNumber([10,4,6,9,3,2,8]))
