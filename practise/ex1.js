ef7726a6a58bb9f32606d0fc26f9c83def676bb1
roshanjha13 <jharoshan618@gmail.com>
2025-06-12 22:37:23 +0530

    count digit

---
+++ practise/ex1.js
@@ -0,0 +1,41 @@
+let n = 4
+
+// for (let i = 0; i < n; i++) {
+//     let row = ''
+//     for (let j = 0; j < n; j++) {
+//         row = row + '*'
+//     }
+    
+//     console.log(row);
+    
+// }
+
+// for (let i = 0; i < n; i++) {
+//     let row = ''
+//     for (let j = 0; j <= i; j++) {
+//         row = row + '*'
+//     }
+    
+//     console.log(row);
+    
+// }
+
+// for (let i = 0; i < n; i++) {
+//     let row = ''
+//     for (let j = 0; j <= i; j++) {
+//         row = row + (i+1)
+//     }
+    
+//     console.log(row);
+    
+// }
+
+// for (let i = 0; i < n; i++) {
+//     let row = ''
+//     for (let j = 0; j <= i; j++) {
+//         row = row + (j+1)
+//     }
+    
+//     console.log(row);
+    
+// }
