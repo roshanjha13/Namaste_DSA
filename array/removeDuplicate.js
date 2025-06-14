5768777a66a93eb8b16c741133c86a2117aee7eb
roshanjha13 <jharoshan618@gmail.com>
2025-06-15 00:09:42 +0530

    remove duplicates from an array

--- array/removeDuplicate.js
+++ array/removeDuplicate.js
@@ -1,16 +1,23 @@
 function removeDuplicate(arr) {
-    let uniqueElem = []
+//my soln
+    // let uniqueElem = []
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (!uniqueElem.includes(arr[i])) {
+    //         uniqueElem.push(arr[i])
+    //     }
+    // }
+    // return uniqueElem;
+    let x = 0
     for (let i = 0; i < arr.length; i++) {
-        if (!uniqueElem.includes(arr[i])) {
-            uniqueElem.push(arr[i])
+        if (arr[i] > arr[x]) {
+            x = x+1;
+            arr[x] = arr[i]
         }
     }
-    
-    let k = uniqueElem.length;
 
-    for (let i = 0; i < k; i++) {
-        
-    }
+    return x+1
 }
 
-removeDuplicate([0,0,1,1,1,2,2,3,3,4])
+let ans = removeDuplicate([0,0,1,1,1,2,2,3,3,4])
+
+console.log(ans);
