ef6a0625c322cf699d6b60e344bfc7ffb4518590
roshanjha13 <jharoshan618@gmail.com>
2025-06-20 22:57:17 +0530

    missing number

--- array/consecutiveOnes.js
+++ array/consecutiveOnes.js
@@ -1,20 +1,36 @@
 function consecutiveOnes(arr) {
+// my solution
+    // let currentCount = 0;
+    // let maxCount = 0;
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] == 1) {
+    //         currentCount = currentCount+1;
+    //         if (currentCount>maxCount) {
+    //             maxCount = currentCount
+    //         }
+    //     } else if (arr[i] !== 1) {
+    //         currentCount = 0
+    //     }
+        
+    // }
+
+    // return maxCount
+// akshay saini solution
     let currentCount = 0;
     let maxCount = 0;
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] == 1) {
-            currentCount = currentCount+1;
-            if (currentCount>maxCount) {
-                maxCount = currentCount
-            }
-        } else if (arr[i] !== 1) {
+            currentCount = currentCount++;
+        } else {
+            maxCount = Math.max(currentCount,maxCount)
             currentCount = 0
         }
         
     }
 
-    return maxCount
+    return Math.max(maxCount,currentCount)
 }
 
 var a = consecutiveOnes([1,1,0,1,1,1,0,1])
---
+++ array/missingNumber.js
@@ -0,0 +1,17 @@
+function missingNumber(nums) {
+    let len = nums.length;
+    let sum = (len*(len+1))/2
+    let sum1 = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum1+=nums[i]
+    }
+
+    return sum - sum1
+    
+    
+}
+
+var a = missingNumber([1,0,4,2,5]) 
+console.log(a);
+
