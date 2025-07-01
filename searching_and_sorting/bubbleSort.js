29b1a3711e2fe63f66f9b2444e6e67ecddc8837f
roshanjha13 <jharoshan618@gmail.com>
2025-07-01 22:21:17 +0530

    bubble sort

---
+++ searching_and_sorting/bubbleSort.js
@@ -0,0 +1,25 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        //track the swapping
+        let isSwapped = false;
+
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j]>arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                isSwapped = true;
+            }
+
+            if (!isSwapped) break;
+        }
+        
+    }
+
+    return arr
+}
+
+console.log(bubbleSort([5,2,4,1]));
