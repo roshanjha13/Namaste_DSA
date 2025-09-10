50c40f3b8fef88e3b3da8699d6304460f5b474e5
roshanjha13 <jharoshan618@gmail.com>
2025-09-10 23:54:26 +0530

    finding peak element

---
+++ binarysearch/peakElement.js
@@ -0,0 +1,17 @@
+function peakElement(arr) {
+    let l = 0;
+    let r =  arr.length -1;
+    while (l < r) {
+        let m = l + Math.floor((r-l)/2);
+
+        if(arr[m] < arr[m+1]){
+            l = m+1;
+        } else {
+            r = m
+        }
+    }
+
+    return l;
+}
+
+console.log(peakElement([1,2,3,1]));
